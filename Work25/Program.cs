f40c31c81ec4633ebae6d80999ed3ae61ef14a5b
KrisanovaOM <fentifluyshka@gmail.com>
2022-10-02 20:29:28 +0300

    Initial commit

---
+++ Work25/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//Метод ввода двух чисел а и b
+int GetNumber (string name)
+{
+  Console.WriteLine("Введите число " + name);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = GetNumber("a");
+int b = GetNumber("b");
+
+//Метод возведения числа a в степень b
+int DegreeNumber(int a, int b)
+{
+  int result = 1;
+  for (int i = 1; i <= b; i++)
+  {
+    result *= a;
+  }
+  return result;
+}
+
+Console.WriteLine("Число " + a + " в натуральной степени "  + b + " равно " + DegreeNumber(a, b));
+
---
+++ Work27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Метод ввода числа a
+int GetNumber()
+{
+  Console.WriteLine("Введите число: ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigit(int number)
+{
+  int sum = 0;
+  for (int i = 0; number > 0; i++)
+  {
+    sum = sum + number % 10;
+    number = number / 10;
+  }
+  return sum;
+}
+
+int a = GetNumber();
+Console.WriteLine("Сумма цифр в числе " + a + ": " + SumDigit(a));
